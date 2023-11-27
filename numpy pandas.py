fe04b4428e81f8bfceff6c0e078bbc1ad901d2d2
Sv140614 <nailya.ramazanova@mail.ru>
2023-11-27 21:16:25 +0400

    first commit
    first commit

--- numpy pandas.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-data=pd.read_csv("https://video.ittensive.com/python-advanced/data-5283-2019-10-04.utf.csv", delimiter=";")
-pd.set_option('display.max_columns', None)
-a=data["Calls"].mean()
-a=round(a)
-print(a)
