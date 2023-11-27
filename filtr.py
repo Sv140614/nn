c340f75ab8b809ca2974b35713ec71589818c73e
Sv140614 <nailya.ramazanova@mail.ru>
2023-11-27 21:19:28 +0400

    first commit
    first commit

--- filtr.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-data=pd.read_csv("http://video.ittensive.com/python-advanced/data-9753-2019-07-25.utf.csv", delimiter=";")
-pd.set_option('display.max_columns', None)
-data["Pros"]=data.apply(lambda x: 100*x[6]/x[7], axis=1) #data["Pros"]=data["UnemployedDisabled"]/data["UnemployedTotal"]*100
-data=data[data["Pros"]<2]
-data=data.sort_values("Year")
-print(data.iloc[0, 2])
